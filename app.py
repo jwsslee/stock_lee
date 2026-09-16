a364d0bb752680ff443f7af8b87b894ee94ff584
jwsslee <jwlee@hallym.ac.kr>
2026-09-17 00:11:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -22,13 +22,13 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error
 from sklearn.model_selection import TimeSeriesSplit
 
 # ================= 사용자 설정: 아래 네 값을 직접 입력 =================
-APP_KEY = os.environ.get('X_APP_KEY')
+APP_KEY = os.environ.get("X_APP_KEY")
 
-APP_SECRET = os.environ.get('X_APP_SECRET')
+APP_SECRET = os.environ.get("X_APP_SECRET")
 
-CANO = '50205776'
+CANO = "50205776"
 
-ACNT_PRDT_CD = '01'
+ACNT_PRDT_CD = "01"
 
 IS_PAPER = True            # True: 모의투자 / False: 실전(조회만 수행)
 CASH_REFERENCE_CODE = "005930"  # 주문가능현금 조회 기준 종목
